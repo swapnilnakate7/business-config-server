ce1416c7957d324b165de08bc9cea41733842b4b
swapnilnakate <nakate.swapnil7@gmail.com>
2021-08-19 17:08:14 +0530

    initial commot

---
+++ src/main/java/in/swapnil/configserver/BusinessApiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package in.swapnil.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BusinessApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/swapnil/configserver/BusinessApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.swapnil.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
